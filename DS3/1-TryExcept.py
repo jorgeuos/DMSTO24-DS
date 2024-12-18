8ec4c5b744866904d98826aebefa5eb1e2e574d5
Jorgeuos <hey@jorgeuos.com>
2024-12-18 09:37:27 +0100

    DS3 TryExcept forts

--- DS3/1-TryExcept.py
+++ DS3/1-TryExcept.py
@@ -22,6 +22,43 @@ except:
    print("Ett fel uppstod 3.")
 
 # Bör generera ett fel
-print(y)
+# print(y)
+
+print("Fånga specifika fel")
+
+try:
+   x = int("hej")
+except ValueError:
+   print("Ett värdefel uppstod.")
+
+# Vi kan hanter flera olika fel
+try:
+    x = int("hej")
+    y = 0
+    print(x / y)
+except ZeroDivisionError:
+    print("Ett ZeroDivisionError fel uppstod.")
+except ValueError:
+   print("Ett värdefel uppstod.")
 
 
+try:
+    x = int("123")
+    y = 0
+    print(x / y)
+except ZeroDivisionError:
+    print("Ett ZeroDivisionError fel uppstod.")
+except ValueError:
+   print("Ett värdefel uppstod.")
+
+try:
+   x = 10 / 0
+except ZeroDivisionError:
+   print("Du kan inte dela med 0.")
+finally:
+   print("Programmet fortsätter.")
+
+x = 10 / 0
+
+# Kommer inte att nå hit ner
+print("Men jag vill ju fortsätta")
