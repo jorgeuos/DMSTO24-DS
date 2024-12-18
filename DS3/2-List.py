79bdb2df2948eaa8ebaf5b7b8f7012923b8427a8
Jorgeuos <hey@jorgeuos.com>
2024-12-18 19:45:41 +0100

    DS3 Uppdaterat med det sista

--- DS3/2-List.py
+++ DS3/2-List.py
@@ -59,7 +59,10 @@ print(reallyLongList[4:])
 print(reallyLongList[:14])
 print(reallyLongList[3:14])
 print(reallyLongList[-1:0])
-# print(reallyLongList.reverse())
+
+reallyLongList.reverse()
+
+print(reallyLongList)
 
 
 my_list = ["äpple", "banan", "körsbär"]
@@ -86,3 +89,77 @@ for number in reallyLongList:
 for number in reallyLongList:
     if number % 2 != 0:
         print(number)
+
+
+# Reverse
+
+my_list = ["äpple", "banan", "körsbär"]
+
+my_list.reverse()
+print(my_list)
+
+reallyLongList = ["äpple", "banan", "körsbär", "druva", "apelsin", "päron", "kiwi", "mango", "passionsfrukt", "ananas"]
+
+print("Length: ")
+print(len(reallyLongList))
+
+x = 1
+for i, fruit in enumerate(reallyLongList):
+    if i == (i+1):
+        continue
+    if (i+1) % 3 == 0:
+        print(fruit)
+    # x += 1
+    x = x + 1
+    # Funkar inte
+    # x++
+
+print(type(x))
+print("x är:")
+print(x)
+
+# Unset variable
+x = None
+
+print(my_list)
+my_list[1] = "blåbär"
+print(my_list)
+
+my_list.insert(1, "orange")
+print(my_list)
+
+my_list.append("jordgubbe")
+my_list.append("blåbär")
+my_list.append("blåbär")
+my_list.append("blåbär")
+my_list.append("Blåbär")
+my_list.append("BlåBär")
+
+# Funkar inte
+# my_list.prepend("hallon")
+
+# Bygger en egen prepend-funktion
+def prepend(prependList, value):
+    prependList.insert(0, value)
+
+prepend(my_list, "hallon")
+
+print(my_list)
+
+try:
+    my_list.remove("blåbär")
+    my_list.remove("blåbär")
+    my_list.remove("blåbär")
+    my_list.remove("blåbär")
+    my_list.remove("blåbär")
+    my_list.remove("blåbär")
+    my_list.remove("blåbär")
+    my_list.remove("blåbär")
+except ValueError:
+    print("Det gick inte att ta bort blåbär")
+
+print(my_list)
+
+my_list.pop()
+
+print(my_list)
