ef812f91abddf5c744b513d418834dbf51a47c86
Jorgeuos <hey@jorgeuos.com>
2024-12-20 10:08:33 +0100

    Laddar upp DS4

---
+++ DS4/02-listTuplerDict.py
@@ -0,0 +1,12 @@
+
+# Skapa en lista med 5 namn.
+# Lägg till ett namn och ta bort ett annat.
+# Iterera genom listan och skriv ut alla namn.
+# Tuppel för koordinater:
+# Skapa en tuppel med tre koordinater (x, y, z).
+# Försök att ändra ett värde och notera vad som händer.
+# Dictionary för en produkt:
+# Skapa en dictionary för en produkt med namn, pris och lager.
+# Uppdatera lager och lägg till en nyckel för kategori.
+
+
