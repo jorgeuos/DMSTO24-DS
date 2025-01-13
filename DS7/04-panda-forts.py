52d3e011e17097b98e878de2b6670489376afc63
Jorgeuos <hey@jorgeuos.com>
2025-01-13 13:14:17 +0100

    Matplotlib exempel

--- DS7/04-panda-forts.py
+++ DS7/04-panda-forts.py
@@ -11,5 +11,23 @@ print(grouped)
 
 
 
+# Från DS6 - Lägga till kolumner
+data = {
+   "Name": ["Anna", "Björn", "Cecilia"],
+   "Age": [25, 30, 27],
+   "City": ["Stockholm", "Göteborg", "Malmö"]
+}
+
+df2 = pd.DataFrame(data)
+
+# Lägg till en ny kolumn
+df2["Age_in_10_years"] = df2["Age"] + 10
+print(df2)
+
+# Ta bort en kolumn
+print("Utan kolumn city")
+df2 = df2.drop(columns=["City"])
+print(df2)
+
 
 
---
+++ DS7/05-matplotlib.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+x_vals = np.linspace(0, 20, 20)
+print(x_vals)
+y_vals = [math.sqrt(i) for i in x_vals]
+plt.plot(x_vals, y_vals)
+
+plt.show()
+
